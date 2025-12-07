76823410a429e5105d6ce41a7dd02bef8db850d2
Raj1928 <rajshinde1928@gmail.com>
2025-12-07 12:52:32 +0530

    update path to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/viransh-veg-restaurant/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
